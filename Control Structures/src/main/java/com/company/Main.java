ea397fa9e355fabdd6e0c46b8fc608f977c7c998
gjlobos <gjlobos141@gmail.com>
2023-07-23 18:24:21 -0300

    Pushing changes to Control Structures code

--- Control Structures/src/main/java/com/company/Main.java
+++ Control Structures/src/main/java/com/company/Main.java
@@ -6,9 +6,13 @@ public class Main {
         for(int i = 1; i <= 5; i++) {
             System.out.println("Hello World");
         }
-        System.out.println("");
+        System.out.println(" ");
 
-        //Ejercicio estructuras playground
+        //Control Structures example
+        //Define two integers. Assign a value to each. Check if a number is
+        //divisible by the second and indicate by means of a message the result obtained.
+        //NOTE: That it is divisible means that when divided it gives an exact value, that is, without
+        //decimals.
         int entero1;
         int entero2;
         int resto;
@@ -16,20 +20,19 @@ public class Main {
         String esDivisibleONo = " es divisible por ";
 
         entero1 = 10;
-        entero2 = 4;
+        entero2 = 5;
         resto = entero1%entero2;
-        mensaje = "El resto = "+resto+". El "+entero1+esDivisibleONo+entero2;
-        System.out.println(mensaje);
                 
-
         if(resto == 0) {
             esDivisibleONo = " es divisible por ";
-            mensaje = "El resto = "+resto+". El "+entero1+esDivisibleONo+entero2;
+            mensaje = "El resto es = "+resto+". El "+entero1+esDivisibleONo+entero2;
             System.out.println(mensaje);
         } else {
             esDivisibleONo = " no es divisible por ";
-            mensaje = "El resto = "+resto+". El "+entero1+esDivisibleONo+entero2;
+            mensaje = "El resto es = "+resto+". El "+entero1+esDivisibleONo+entero2;
             System.out.println(mensaje);
         }
+
+
     }
 }
