1b07434142d4501103874d37ef8436804072ba98
gjlobos <gjlobos141@gmail.com>
2023-07-27 01:23:06 -0300

    Pushing Float Class to remote repository

---
+++ Float Class/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Float coefficient = 2.5f;
+        // Float num = new Float(0.5); // deprecated
+        Float num = 0.5f;
+
+        System.out.println(coefficient);
+        System.out.println(num);
+
+    }
+}
